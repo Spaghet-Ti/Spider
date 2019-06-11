80df8453da96549b3111d922c742fcbec5108f07
Spaghet-Ti <spaghet-ti@hotmail.com>
2019-06-10 21:39:02 -0400

    Switching to Netlify
    It is hard to work with the subdirectories.

---
+++ build/precache-manifest.9fffef0a91be4a7a6e14146ae1f62519.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "717fd56290529819ccb39dc17a8dcce8",
+    "url": "/Spiders-Tarantulas/static/media/EX.717fd562.png"
+  },
+  {
+    "revision": "8b5734ad93308659777f",
+    "url": "/Spiders-Tarantulas/static/css/main.c46d46ea.chunk.css"
+  },
+  {
+    "revision": "87398af43638daecc601bf9047a45d00",
+    "url": "/Spiders-Tarantulas/static/media/VU.87398af4.png"
+  },
+  {
+    "revision": "4a472b26e386b24aae17",
+    "url": "/Spiders-Tarantulas/static/js/1.4a472b26.chunk.js"
+  },
+  {
+    "revision": "3fca8fc39754ac2599dc",
+    "url": "/Spiders-Tarantulas/static/js/runtime~main.3fca8fc3.js"
+  },
+  {
+    "revision": "547faa6b9135e1bd5befa40a12104e9a",
+    "url": "/Spiders-Tarantulas/static/media/CR.547faa6b.png"
+  },
+  {
+    "revision": "3d3d47bfb23c233e6eaea4b224c75a8f",
+    "url": "/Spiders-Tarantulas/static/media/EN.3d3d47bf.png"
+  },
+  {
+    "revision": "63c556467e78c416308a400a9f8ab6ec",
+    "url": "/Spiders-Tarantulas/static/media/EW.63c55646.png"
+  },
+  {
+    "revision": "8b5734ad93308659777f",
+    "url": "/Spiders-Tarantulas/static/js/main.8b5734ad.chunk.js"
+  },
+  {
+    "revision": "7db9349d4d2241bb5e6bf131b535fef8",
+    "url": "/Spiders-Tarantulas/static/media/LC.7db9349d.png"
+  },
+  {
+    "revision": "06ecc9f2a5f2271a255cc540aad30baa",
+    "url": "/Spiders-Tarantulas/static/media/NE.06ecc9f2.png"
+  },
+  {
+    "revision": "ab775e19925b8d59f6ae5fe677ec498c",
+    "url": "/Spiders-Tarantulas/static/media/NT.ab775e19.png"
+  },
+  {
+    "revision": "ce364db8e191f81d0b7fcf440abb5cab",
+    "url": "/Spiders-Tarantulas/static/media/PE.ce364db8.png"
+  },
+  {
+    "revision": "689e6f85d9318b405f78d74287079d26",
+    "url": "/Spiders-Tarantulas/static/media/PEW.689e6f85.png"
+  },
+  {
+    "revision": "409e2defe09906e517dd3bbd21ff332a",
+    "url": "/Spiders-Tarantulas/static/media/Status.409e2def.png"
+  },
+  {
+    "revision": "4a472b26e386b24aae17",
+    "url": "/Spiders-Tarantulas/static/css/1.44f60170.chunk.css"
+  },
+  {
+    "revision": "bbbc1b5965540975add001fe61481e75",
+    "url": "/Spiders-Tarantulas/index.html"
+  }
+];
